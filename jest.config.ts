a444e5b8f3bece993e6ff246746f60a33b2f470a
Ania Banachowicz <101459177+aniaxb@users.noreply.github.com>
2023-10-07 15:52:52 +0200

    Added Vite to project and configured Jest along with support for svg and css files

---
+++ jest.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: [
+    "<rootDir>/tests/setupTests.ts"
+  ],
+  moduleNameMapper: {
+    "^.+\\.svg$": "jest-svg-transformer",
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy', // Handle CSS files
+  },
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest', // Handle TypeScript files
+  },
+};
+
+export default config;
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+// import logo from '../public/logo512.png';
 import './App.css';
 
 function App() {
