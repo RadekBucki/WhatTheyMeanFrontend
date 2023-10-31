c10e424350b7dfaf27bab73d79059f88c0c76b92
Ania Banachowicz <101459177+aniaxb@users.noreply.github.com>
2023-10-31 22:07:44 +0100

    Redid Transcription Dialog as a separate component

--- src/pages/history/History.tsx
+++ src/pages/history/History.tsx
@@ -1,9 +1,9 @@
 import React, {useState} from 'react';
-import {Button, Dialog, DialogBody, DialogFooter, DialogHeader, Input, Typography} from '@material-tailwind/react';
+import {Button, Input, Typography} from '@material-tailwind/react';
 import {MagnifyingGlassIcon, TrashIcon} from '@heroicons/react/20/solid';
-import {ArrowDownTrayIcon} from '@heroicons/react/24/solid';
+import Transcription from './transcription/Transcription';
 
-interface TranscriptData {
+export interface TranscriptData {
   uid: number,
   name: string,
   start_date: string,
@@ -26,12 +26,13 @@ export default function History() {
     setSearchQuery(e.target.value);
   };
 
-  const handleOpenTranscriptionDialog = (transcript: TranscriptData) => {
-    if (!selectedTranscript) {
-      setSelectedTranscript(transcript);
-    }
-
+  const handleOpenTranscriptionDialog = (transcript: TranscriptData | null) => {
+    setSelectedTranscript(transcript);
     setOpenTranscriptionDialog(true);
+  };
+
+  const handleTranscriptionDialog = () => {
+    setOpenTranscriptionDialog(!openTranscriptionDialog);
   };
 
   return (
@@ -118,49 +119,7 @@ export default function History() {
           </div>
         </div>
       </div>
-
-      <Dialog size="lg" open={openTranscriptionDialog}  handler={handleOpenTranscriptionDialog}>
-        <DialogHeader className="text-3xl justify-center">History - {selectedTranscript?.name}</DialogHeader>
-        <DialogBody className="text-xl text-selected-blue">
-          <div className="flex flex-col md:flex-row items-center gap-4 px-4">
-            <div className={'w-3/4'}>
-              <p className="mb-6">
-                <span className="font-semibold">Start date:</span> {selectedTranscript?.start_date}
-              </p>
-              <p className={'mb-6'}>
-                <span className="font-semibold">Status:</span> {selectedTranscript?.status}
-              </p>
-              <p className={'mb-6'}>
-                <span className="font-semibold">Duration:</span> {selectedTranscript?.duration}
-              </p>
-            </div>
-            <div className="w-2/3">
-              <img className={'rounded-xl'} alt="Audio board image" src="https://images.unsplash.com/photo-1525022340574-113732565927?auto=format&fit=crop&q=80&w=2050&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
-            </div>
-          </div>
-        </DialogBody>
-        <DialogFooter className="flex gap-4 justify-center">
-          <Button
-            onClick={() => {
-              setSelectedTranscript(null);
-              setOpenTranscriptionDialog(false);
-            }}
-            className="flex items-center gap-2 bg-teal hover:bg-teal px-6"
-          >
-            <ArrowDownTrayIcon className={'w-5 h-4'}/>
-            Download
-          </Button>
-          <Button
-            onClick={() => {
-              setSelectedTranscript(null);
-              setOpenTranscriptionDialog(false);
-            }}
-            className="bg-bright-pink hover:bg-bright-pink px-12 "
-          >
-            <span>Cancel</span>
-          </Button>
-        </DialogFooter>
-      </Dialog>
+      <Transcription open={openTranscriptionDialog} handler={handleTranscriptionDialog} selectedTranscript={selectedTranscript} />
     </div>
   );
 }
