28e2078f7f28ce52501fb222d872cc69c470242d
Radosław Bucki <236507@edu.p.lodz.pl>
2023-11-06 22:12:08 +0100

    #6 Send notification hook

---
+++ src/hooks/useSystemNotificationSender.ts
@@ -0,0 +1,19 @@
+import {TranscriptData} from "../types";
+
+export default () => {
+  return {
+    requestPermission: async (): Promise<void> => {
+      await Notification.requestPermission();
+    },
+    sendSystemNotification: async (transcription: TranscriptData): Promise<void> => {
+      if (Notification.permission === 'granted') {
+        const notification = new Notification(`Transcript ${transcription.name} updated`, {
+          body: `Status: ${transcription.status}`,
+          lang: 'en',
+        });
+      } else {
+        alert(`Transcript Status updated: ${transcription.status}.\n\n Please enable notifications in your browser settings.`);
+      }
+    }
+  };
+}
