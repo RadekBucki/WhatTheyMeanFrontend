d305ac70264012abf2c673a6610507bb2c4be10f
Paweł Dera <paweldera01@gmail.com>
2023-12-03 15:16:56 +0100

    Imp transcribe connection

--- src/pages/transcribe/Transcribe.tsx
+++ src/pages/transcribe/Transcribe.tsx
@@ -1,4 +1,4 @@
-import React, {ChangeEvent, useState} from 'react';
+import React, {ChangeEvent, useEffect, useState} from 'react';
 import {
   Button,
   Dialog,
@@ -12,18 +12,50 @@ import {
 import {
   ArrowDownTrayIcon,
   ClipboardDocumentIcon,
-  DocumentMagnifyingGlassIcon,
-  PlayIcon
+  DocumentMagnifyingGlassIcon, GlobeAltIcon
 } from '@heroicons/react/24/solid';
 import useSocketContainer from '../../sockets/UseSocketContainer';
 import {useSocketHooks} from '../../sockets/SocketHooks';
 import {usePostRequests} from '../../communication/network/PostRequests';
 import useAnalyseDataSaver from '../../hooks/useAnalyseDataSaver';
+import {useGetRequests} from '../../communication/network/GetRequests';
+import {Analyse} from '../../communication/Types';
 
 export default function Transcribe() {
-  const [openYoutubeLinkDialog, setOpenYoutubeLinkDialog] = useState(false);
+  const [openWebLinkDialog, setOpenWebLinkDialog] = useState(false);
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
-  const handleOpenYoutubeLinkDialog = () => setOpenYoutubeLinkDialog(!openYoutubeLinkDialog);
+  const [isTranscribing, setIsTranscribing] = useState<boolean>(false);
+  const [webLink, setWebLink] = useState<string>('');
+  const [showError, setShowError] = useState<boolean>(false);
+  const [currentAnalysisID, setCurrentAnalysisID] = useState<string>('');
+  const [currentAnalysisResult, setCurrentAnalysisResult] = useState<Analyse | null>(null);
+  const handleOpenWebLinkDialog = () => setOpenWebLinkDialog(!openWebLinkDialog);
+
+  const handleConfirmWebLinkDialog = () => {
+    setShowError(false);
+
+    if (webLink === '') {
+      setShowError(true);
+      return;
+    }
+
+    setOpenWebLinkDialog(!openWebLinkDialog);
+    setIsTranscribing(true);
+
+    postHook.postRegisterUrl(webLink).then(uuid => {
+      setCurrentAnalysisID(uuid.analysis_uuid);
+      socket.startAnal(uuid.analysis_uuid);
+    });
+  };
+
+  useEffect(() => {
+    if (socketContainer.progress === '100') {
+      useGetRequests().getAnalyze(currentAnalysisID).then(res => {
+        setCurrentAnalysisResult(res);
+      });
+    }
+  }, [useSocketContainer().progress]);
+
   const handleOpenFileBrowser = () => {
     document.getElementById('fileInput')?.click();
   };
@@ -40,19 +72,23 @@ export default function Transcribe() {
     selectedFile;
   };
 
+  const handleCopy = () => {
+    navigator.clipboard.writeText(currentAnalysisResult?.video_summary as string);
+  };
+
   const socketContainer = useSocketContainer();
   const analyseDataSaver = useAnalyseDataSaver();
   const handleSaveTranscriptData = () => analyseDataSaver.saveToPdf({
-    name: 'Analyse 1',
-    start_date: '2023-04-21 13:45:00',
-    finish_date: '2023-04-21 13:48:00',
-    status: 'Success',
-    file_type: 'mp4',
-    link: 'https://www.youtube.com/watch?v=1',
-    raw_file: 'base64',
-    full_transcription: 'Full transcription',
-    video_summary: 'Video summary',
-    author_attitude: 'Author attitude',
+    name: currentAnalysisResult?.name as string,
+    start_date: currentAnalysisResult?.start_date as string,
+    finish_date: currentAnalysisResult?.finish_date as string,
+    status: currentAnalysisResult?.status as string,
+    file_type: currentAnalysisResult?.file_type as string,
+    link: currentAnalysisResult?.link as string,
+    raw_file: currentAnalysisResult?.raw_file as string,
+    full_transcription: currentAnalysisResult?.full_transcription as string,
+    video_summary: currentAnalysisResult?.video_summary as string,
+    author_attitude: currentAnalysisResult?.author_attitude as string,
   });
 
   return (
@@ -68,11 +104,11 @@ export default function Transcribe() {
         </Typography>
 
         <div className={'flex flex-col xl:flex-row justify-center gap-24 mt-24'}>
-          <Button className={'bg-yt w-full max-w-2xl h-[300px] rounded-3xl flex flex-col justify-center'}
-            style={{textTransform: 'none'}} onClick={handleOpenYoutubeLinkDialog}>
-            <PlayIcon className={'text-white w-24 h-24 mx-auto'}/>
-            <Typography className="text-audio-text font-bold text-2xl mt-4 mx-auto">
-              Youtube link
+          <Button className={'bg-dark-blue w-full max-w-2xl h-[300px] rounded-3xl flex flex-col justify-center'}
+            style={{textTransform: 'none'}} onClick={handleOpenWebLinkDialog}>
+            <GlobeAltIcon className={'text-white w-24 h-24 mx-auto'}/>
+            <Typography className="text-white font-bold text-2xl mt-4 mx-auto">
+              Youtube link or TikTok link
             </Typography>
           </Button>
           <Button className={'bg-audio w-full max-w-2xl h-[300px] rounded-3xl flex flex-col justify-center'}
@@ -84,70 +120,76 @@ export default function Transcribe() {
           </Button>
         </div>
 
-        <Typography className="text-selected-blue font-bold text-2xl text-center pt-24 pb-4">
-          Progress
-        </Typography>
-
-        <div className="w-full px-12 2xl:px-64">
-          <div className="mb-2 flex items-center justify-between gap-4">
-            <Typography color="blue-gray" variant="h6">
-              Completed
+        { isTranscribing &&
+          <>
+            <Typography className="text-selected-blue font-bold text-2xl text-center pt-24 pb-4">
+                Progress
             </Typography>
-            <Typography color="blue-gray" variant="h6">
-              {socketContainer.progress}%
+
+            <div className="w-full px-12 2xl:px-64">
+              <div className="mb-2 flex items-center justify-between gap-4">
+                <Typography color="blue-gray" variant="h6">
+                    Completed
+                </Typography>
+                <Typography color="blue-gray" variant="h6">
+                  {socketContainer.progress}%
+                </Typography>
+              </div>
+              <Progress value={parseInt(socketContainer.progress)}/>
+            </div>
+          </>
+        }
+
+        { socketContainer.progress === '100' &&
+          <>
+            <Typography className="text-selected-blue font-bold text-2xl text-center pt-24 pb-6">
+                Analysis
             </Typography>
-          </div>
-          <Progress value={parseInt(socketContainer.progress)}/>
-        </div>
+            <div className="w-full px-12 2xl:px-64 flex flex-col items-end gap-2">
+              <Textarea label="Message" disabled value={currentAnalysisResult?.video_summary}/>
+              <div className={'flex gap-5'}>
+                <Button>
+                  <ClipboardDocumentIcon onClick={handleCopy} className={'w-5 h-5'}/>
+                </Button>
+                <Button onClick={handleSaveTranscriptData}>
+                  <ArrowDownTrayIcon className={'w-5 h-5'}/>
+                </Button>
+              </div>
+            </div>
+          </>
+        }
 
-        <Typography className="text-selected-blue font-bold text-2xl text-center pt-24 pb-6">
-          Analysis
-        </Typography>
-
-        <div className="w-full px-12 2xl:px-64 flex flex-col items-end gap-2">
-          <Textarea label="Message" disabled value={'Your analysis will appear here'}/>
-          <div className={'flex gap-5'}>
-            <Button>
-              <ClipboardDocumentIcon className={'w-5 h-5'}/>
-            </Button>
-            <Button onClick={handleSaveTranscriptData}>
-              <ArrowDownTrayIcon className={'w-5 h-5'}/>
-            </Button>
-          </div>
-        </div>
-
-        <Dialog open={openYoutubeLinkDialog} handler={handleOpenYoutubeLinkDialog}>
-          <DialogHeader>Youtube link</DialogHeader>
+        <Dialog open={openWebLinkDialog} handler={handleOpenWebLinkDialog}>
+          <DialogHeader>Youtube link or TikTok link</DialogHeader>
           <DialogBody>
             <p>
-              Please paste the YouTube link in the input field below.
+              Please paste the YouTube link or TikTok link in the input field below.
             </p>
             <p className={'mb-6'}>
-              Example: https://www.youtube.com/watch?v=dQw4w9WgXcQ
+              Example: https://www.youtube.com/watch?v=dQw4w9WgXcQ or https://www.tiktok.com/@dzidzia_dziadzia/video/7271698811439828256
             </p>
-            <Input variant='outlined' label='Youtube link' crossOrigin={undefined}/>
+            <Input error={showError} value={webLink} onChange={(event) => { setWebLink(event.target.value); }} variant='outlined' label='Youtube link or TikTok link' crossOrigin={undefined}/>
           </DialogBody>
           <DialogFooter>
             <Button
               variant="text"
               color="red"
-              onClick={handleOpenYoutubeLinkDialog}
+              onClick={handleOpenWebLinkDialog}
               className="mr-1"
             >
               <span>Cancel</span>
             </Button>
-            <Button variant="gradient" color="green" onClick={handleOpenYoutubeLinkDialog}>
+            <Button variant="gradient" color="green" onClick={handleConfirmWebLinkDialog}>
               <span>Confirm</span>
             </Button>
           </DialogFooter>
-        </Dialog>
-        <input
+        </Dialog><input
           id='fileInput'
           type='file'
           accept='.mp3,.mp4'
           style={{display: 'none'}}
-          onChange={handleFileSelected}
-        />
+          onChange={handleFileSelected}/>
+
       </div>
     </div>
   );
